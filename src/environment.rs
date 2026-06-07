8a5f640d56c1b4b6bb6c8315fd7ef012861f4691
SrGesus <108523575+SrGesus@users.noreply.github.com>
2026-06-08 00:54:31 +0100

    Add environment with jinja and tailwind

---
+++ src/environment.rs
@@ -0,0 +1,97 @@
+use std::{
+    path::PathBuf,
+    sync::{Arc, Mutex, RwLock},
+};
+
+use itertools::Itertools;
+use regex::Regex;
+use tailwind_css::TailwindBuilder;
+use tokio::fs::{self};
+use tracing::Level;
+
+#[derive(Debug)]
+pub struct Environment(pub RwLock<EnvironmentInner>);
+
+#[derive(Debug)]
+pub struct EnvironmentInner {
+    pub jinja: minijinja::Environment<'static>,
+    pub tailwind: TailwindBuilder,
+    pub tailwind_parsed: String,
+}
+
+impl Environment {
+    pub async fn build(templates_path: &str) -> Arc<Environment> {
+        let span = tracing::span!(
+            Level::INFO,
+            "Environment::build",
+            templates_path = templates_path
+        );
+        let _enter = span.enter();
+        let mut stack = vec![PathBuf::from(templates_path)];
+        let mut handles = vec![];
+        let env = Arc::new(Environment(RwLock::new(EnvironmentInner {
+            jinja: minijinja::Environment::new(),
+            tailwind: TailwindBuilder::default(),
+            tailwind_parsed: String::new(),
+        })));
+
+        while let Some(dir) = stack.pop() {
+            let mut dir = fs::read_dir(dir).await.unwrap();
+
+            while let Some(entry) = dir.next_entry().await.unwrap() {
+                let metadata = entry.metadata().await.unwrap();
+                if metadata.is_dir() {
+                    stack.push(entry.path());
+                } else if metadata.is_file() {
+                    handles.push(tokio::spawn(Self::handle_file(entry.path(), env.clone())));
+                }
+            }
+        }
+
+        for handle in handles {
+            handle.await.unwrap();
+        }
+
+        {
+            let mut guard = env.0.write().unwrap();
+            guard.tailwind_parsed = guard.tailwind.bundle().unwrap();
+        }
+
+        env
+    }
+
+    async fn handle_file<'a>(path: PathBuf, env: Arc<Environment>) {
+        let template_contents: String = fs::read_to_string(&path).await.unwrap();
+        let re: regex::Regex = Regex::new(r#"class="([\w\/:\-\s]+)""#).unwrap();
+
+        // Get all css classes for tailwind
+        let classes = re
+            .captures_iter(&template_contents)
+            .map(|c| {
+                let (_, [classes]) = c.extract();
+                classes
+            })
+            .join(" ");
+        if !classes.is_empty() {
+            let mut e = env.0.write().unwrap();
+
+            for c in classes.split_whitespace() {
+                match e.tailwind.trace(c, false).ok() {
+                    Some(_) => tracing::debug!("Seen tailwind class: {}", c),
+                    None => tracing::debug!("Seen non-tailwind class: {}", c),
+                }
+            }
+        }
+
+        let template_name = path.to_string_lossy().to_string();
+        tracing::debug!("Adding jinja template: {}", template_name);
+
+        // Load templates into jinja
+        env.0
+            .write()
+            .unwrap()
+            .jinja
+            .add_template_owned(template_name, template_contents)
+            .unwrap();
+    }
+}
---
+++ src/router.rs
@@ -0,0 +1,7 @@
+use std::sync::{Arc, RwLock};
+
+use axum::Router;
+
+use crate::environment::Environment;
+
+
