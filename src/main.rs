62026329a16fc4a13ea6adfd7d6f26abe2101828
SrGesus <108523575+SrGesus@users.noreply.github.com>
2026-06-08 17:24:42 +0100

    Add static assets folder

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,3 @@
-use std::{
-    os::linux::raw::stat,
-    path::PathBuf,
-    sync::{Arc, RwLock},
-};
-
-use axum::{Router, extract::State, routing::get};
-use itertools::Itertools;
-use regex::Regex;
-use tailwind_css::TailwindBuilder;
-use tokio::fs::{self};
 use tracing_subscriber::fmt;
 
 mod environment;
@@ -16,12 +5,6 @@ mod router;
 
 use environment::Environment;
 
-use crate::environment::EnvironmentInner;
-
-#[axum::debug_handler]
-async fn get_tailwind(state: State<Arc<Environment>>) -> String {
-    state.0.0.read().unwrap().tailwind_parsed.clone()
-}
 
 #[tokio::main]
 async fn main() {
@@ -33,16 +16,14 @@ async fn main() {
         .with_max_level(tracing::Level::DEBUG)
         .init();
 
-    println!("{:?}", Environment::build("./templates").await);
     let env = Environment::build("./templates").await;
 
-    let app = Router::new()
-        .route("/assets/tailwind.css", get(get_tailwind))
-        .with_state(env);
+    let app = router::router(env);
 
     // let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
     // // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    tracing::info!("Landing running at http://{}/", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
--- src/router.rs
+++ src/router.rs
@@ -1,7 +1,18 @@
-use std::sync::{Arc, RwLock};
+use std::sync::Arc;
 
-use axum::Router;
+use axum::{Router, extract::State, routing::get};
+use tower_http::services::ServeDir;
 
 use crate::environment::Environment;
 
+#[axum::debug_handler]
+async fn get_tailwind(state: State<Arc<Environment>>) -> String {
+    state.0.0.read().unwrap().tailwind_parsed.clone()
+}
 
+pub(crate) fn router(state: Arc<Environment>) -> Router {
+    Router::new()
+        .route("/assets/tailwind.css", get(get_tailwind))
+        .nest_service("/assets/", ServeDir::new("assets"))
+        .with_state(state)
+}
